1dfda6d4b8b3d9294b1048726745163c8e2fb2c3
joaopedrorori <joaopedrorr.df@gmail.com>
2025-05-22 12:21:51 -0300

    projeto finalizado, eu acho

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,24 @@ const btn = document.getElementById("goUpBtn");
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  console.log("cliock");
 });
 
-// function botaoaparece() {
-//   btn.classList.toggle("visible", window.scrollY >= 600);
-// }
-// botaoaparece();
-
-// addEventListener("scroll", () => console.log(window.scrollY));
-//600
+const menuMobileBtn = document.querySelector(".navbar-btn");
+function abreMenu(event) {
+  const navBar = document.querySelector(".navbar");
+  const menu = document.querySelector(".navbar-menu");
+  const active = navBar.classList.contains("active");
+  console.log(event.target);
+  menu.addEventListener("click", (e) => {
+    const fatherElement = e.target.parentNode.parentNode;
+    if (fatherElement === menu) {
+      navBar.classList.remove("active");
+    }
+  });
+  if (active) {
+    navBar.classList.remove("active");
+  } else {
+    navBar.classList.add("active");
+  }
+}
+menuMobileBtn.addEventListener("pointerdown", abreMenu);
